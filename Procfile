ab691a3f277c35f4e8160200ed10f2df46b01c75
Gaurav Jain <gjain369@gmail.com>
2022-08-22 23:12:43 +0530

    sorry i messed up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn horc_elections.wsgi
--- horc_elections/settings.py
+++ horc_elections/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(Path.joinpath(BASE_DIR, '.env'))
 
-SECRET_KEY = env(SECRET_KEY)
+SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 ALLOWED_HOSTS = []
 
